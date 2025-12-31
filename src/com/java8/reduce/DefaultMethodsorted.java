b4e3f3ef3e3f1f81d7bb5944e1a841dd06556514
sachin <sachinorsharma@gmail.com>
2026-01-01 00:03:10 +0530

    update file

--- src/com/java8/reduce/DefaultMethodsorted.java
+++ src/com/java8/reduce/DefaultMethodsorted.java
@@ -9,10 +9,10 @@ import java.util.List;
 public class DefaultMethodsorted {
 public static void main(String[] args) {
 	List<Integer> listarray = Arrays.asList(2,325,4,78,60);
-	listarray.sort(Comparator.naturalOrder());
+	
 	System.out.println(listarray);
 	System.out.println("-----reverse order==========");
-	listarray.sort(Comparator.reverseOrder());
+	
 	System.out.println(listarray);
 
 	}
