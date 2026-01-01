a7cb2bcfd592c8be354dccee39557722616c9b43
sachin <sachinorsharma@gmail.com>
2026-01-02 03:43:01 +0530

    modified count

--- src/com/java8/Count_StreamElemeent_using_counting.java
+++ src/com/java8/Count_StreamElemeent_using_counting.java
@@ -7,6 +7,7 @@ public class Count_StreamElemeent_using_counting {
 public static void main(String[] args) {
 	// creating a stream of strings
     Stream<String> s = Stream.of("1", "2", "3", "4");
+    System.out.println("stream "+s.toString());
 
     // using Collectors counting() method to
     // count the number of input elements
